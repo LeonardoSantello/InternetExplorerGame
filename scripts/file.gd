4d8176b51c480f5e56a06e7f34293bcecd6f30e8
arthur43613 <arthur.valim@sou.unifeob.edu.br>
2025-09-12 16:12:15 -0300

    fisica para pegar itens

--- scripts/file.gd
+++ scripts/file.gd
@@ -1,4 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body: CharacterBody2D):
-		get_parent().get_node("sfx_coin").play() #pega o som do node pai para tocar
+		get_parent().get_node("sfx_coin").play()  # toca o som do node pai
+		queue_free()                               
