e43bd6d3d7ce2cdce0467759fb2cae8ee744f015
arthur43613 <arthur.valim@sou.unifeob.edu.br>
2025-09-23 20:53:01 -0300

    feito um menu principal para o jogo

---
+++ Menu principal/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+
+func _ready() -> void:
+	pass 
+
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/test_area.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menu principal/opcoes.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
